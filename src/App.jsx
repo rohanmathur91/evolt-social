5e664aac0693e16713fd9d3cf5eb874d5dc7f8d3
rohanmathur91 <mathurrohan04@gmail.com>
2022-04-20 13:55:05 +0530

    chore: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Welcome back</h1>
+      <h2>This evolt social</h2>
+    </div>
+  );
+};
+
+export default App;
