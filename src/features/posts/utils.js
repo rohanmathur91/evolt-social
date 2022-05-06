dcccd5c3e26e89b27645a3f75d5f7bbd6621269a
rohanmathur91 <mathurrohan04@gmail.com>
2022-05-06 12:40:19 +0530

    refactor: add doc comments for utils

--- src/features/posts/utils.js
+++ src/features/posts/utils.js
@@ -1,12 +1,35 @@
+/**
+ * @description Takes a date as string and returns formatted date.
+ *
+ * @param  {string} updatedDate
+ * @returns  {string} formatted date
+ */
+
 export const getDate = (updatedDate) => {
   let date = new Date(updatedDate);
   date = String(date).split(" ");
   return `${date[1]} ${date[2]}`;
 };
 
+/**
+ * @description This function gives the status of post liked by user or not.
+ *
+ * @param  {object} user
+ * @param  {object} likes
+ * @returns {boolean} status
+ */
+
 export const getPostLikedStatus = (user, likes) => {
   return likes?.likedBy.some(({ username }) => username === user?.username);
 };
+
+/**
+ * @description This function returns the single post depending on postId.
+ *
+ * @param  {array} posts
+ * @param  {string} postId
+ * @returns {object} post
+ */
 
 export const getSinglePost = (posts, postId) => {
   return posts.find(({ _id }) => _id === postId);
