7de65240e738e6aa5539c30107caa75509a15ad3
rohanmathur91 <mathurrohan04@gmail.com>
2022-05-06 17:30:49 +0530

    refactor: sidebar static data

---
+++ src/common/data.js
@@ -0,0 +1,8 @@
+export const sideBarData = [
+  { path: "/", icon: "home", text: "Feed" },
+  { path: "/search", icon: "search", text: "Search" },
+  { path: "/explore", icon: "explore", text: "Explore" },
+  { path: "/notifications", icon: "notifications", text: "Notification" },
+  { path: "/bookmarks", icon: "bookmark_border", text: "Bookmarks" },
+  { path: "/profile/1", icon: "perm_identity", text: "Profile" },
+];
