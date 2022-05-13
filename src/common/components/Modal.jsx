75277a311a780ead54d88db4363d4e467e779689
rohanmathur91 <mathurrohan04@gmail.com>
2022-05-14 00:29:14 +0530

    fix(style): modal close click issue

--- src/common/components/Modal.jsx
+++ src/common/components/Modal.jsx
@@ -20,7 +20,10 @@ export const Modal = ({ children, handleModalType }) => {
       onClick={(e) => handleModalType("", mouseUpTarget, mouseDownTarget)}
       className="flex flex-row justify-center fixed top-0 bottom-0 right-0 left-0 h-screen w-screen z-10 backdrop-blur-[1px] bg-black/40"
     >
-      <div className="mx-4 mt-20" onClick={(e) => e.stopPropagation()}>
+      <div
+        className="mx-4 mt-20 self-start"
+        onClick={(e) => e.stopPropagation()}
+      >
         {children}
       </div>
     </div>,
