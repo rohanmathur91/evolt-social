72b5c97c8794e7e3d14d1eaaead20faab51acb69
rohanmathur91 <mathurrohan04@gmail.com>
2022-05-13 18:56:34 +0530

    fix: explore feed posts

--- src/features/feed/Explore.jsx
+++ src/features/feed/Explore.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { CircularLoader, Sidebar, TopContributors } from "../../common";
 import { useAuth } from "../auth";
-import { PostCard, usePosts } from "../posts";
+import { getPosts, PostCard, usePosts } from "../posts";
 import { useProfile } from "../profile";
 import { getExploreFeed } from "./utils";
+import { CircularLoader, Sidebar, TopContributors } from "../../common";
 
 export const Explore = () => {
   const { user } = useAuth();
+  const dispatch = useDispatch();
   const { posts, isLoading } = usePosts();
   const { loggedInUserfollowings } = useProfile();
   const exploreFeed = getExploreFeed(user, posts, loggedInUserfollowings);
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <div className="grid-container">
