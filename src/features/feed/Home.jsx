8c6b13bb1aab8d26d4a96562070b61a74a0a2359
rohanmathur91 <mathurrohan04@gmail.com>
2022-05-14 11:56:32 +0530

    fix: home feed layout for add post

--- src/features/feed/Home.jsx
+++ src/features/feed/Home.jsx
@@ -4,12 +4,19 @@ import { useDispatch } from "react-redux";
 import { useAuth } from "../auth";
 import { useProfile } from "../profile";
 import { getPosts, usePosts, PostCard } from "../posts";
-import { Sidebar, TopContributors, CircularLoader } from "../../common";
+import {
+  Sidebar,
+  useModal,
+  TopContributors,
+  CircularLoader,
+  POSTMODAL,
+} from "../../common";
 import { getHomeFeed, getPostsBySortType } from "./utils";
 
 export const Home = () => {
   const { user } = useAuth();
   const dispatch = useDispatch();
+  const { handleModalType } = useModal();
   const { posts, isLoading } = usePosts();
   const { loggedInUserfollowings } = useProfile();
   const [sortType, setSortType] = useState("");
@@ -29,10 +36,52 @@ export const Home = () => {
       <Sidebar />
       <TopContributors />
       <main className="main pb-12 px-2 md:px-0">
-        <div className="mt-6 md:mt-0 max-w-xl mx-auto">
-          <h4 className="font-semibold text-blue-500 mb-4">User Feed</h4>
-          <div className="flex gap-2 my-6 md:mt-0">
+        <div className="mt-6 md:mt-0 max-w-xl mx-auto mb-4">
+          <h4 className="font-semibold text-blue-500 mb-4 text-center">
+            User Feed
+          </h4>
+
+          <button
+            title="Add post"
+            onClick={() => handleModalType(POSTMODAL)}
+            className="bg-white w-full p-2 rounded-lg border"
+          >
+            <span className="flex items-center">
+              {user?.profileImage ? (
+                <img
+                  loading="lazy"
+                  src={user.profileImage?.url || ""}
+                  alt={user.profileImage?.original_filename || ""}
+                  className="w-11 h-11 md:w-12 md:h-12 mr-3 object-cover rounded-full bg-gray-200"
+                />
+              ) : (
+                <span className="w-11 h-11 md:w-12 md:h-12 text-xl mr-3 flex flex-shrink-0 items-center justify-center font-semibold rounded-full bg-blue-500 text-white">
+                  {user?.firstName[0].toUpperCase()}
+                </span>
+              )}
+
+              <span className="text-gray-400 border p-2 w-full text-left rounded-lg">
+                What's on your mind?
+              </span>
+            </span>
+            <span className="flex items-center ml-2 pt-2">
+              <span className="material-icons-outlined pt-[2px] hover:cursor-pointer text-2xl text-blue-500">
+                add_reaction
+              </span>
+              <span className="text-sm text-gray-400 ml-2 mr-4">Reactions</span>
+              <span className="material-icons-outlined hover:cursor-pointer text-2xl text-blue-500">
+                add_photo_alternate
+              </span>
+              <span className="text-sm text-gray-400 ml-2">Photo</span>
+            </span>
+          </button>
+
+          <div className="flex items-center gap-2 my-6">
+            <span className="flex-shrink-0 text-sm text-gray-500">
+              Sort by:
+            </span>
             <button
+              title="Sort by Recent"
               onClick={() => handleSortTypeClick("SORT_BY_RECENT")}
               className="btn w-full flex items-center justify-center font-semibold py-2 px-4 bg-blue-100 text-blue-500 rouded text-sm"
             >
@@ -42,6 +91,7 @@ export const Home = () => {
               Recent
             </button>
             <button
+              title="Sort by Trending"
               onClick={() => handleSortTypeClick("SORT_BY_TRENDING")}
               className="btn w-full flex items-center justify-center font-semibold py-2 px-4 bg-blue-100 text-blue-500 rouded text-sm"
             >
