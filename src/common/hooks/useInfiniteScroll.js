8cea63e2bea95995cc6fe517f49229528a62a27d
rohanmathur91 <mathurrohan04@gmail.com>
2022-05-16 16:31:01 +0530

    fix: infinite-scroll hook

--- src/common/hooks/useInfiniteScroll.js
+++ src/common/hooks/useInfiniteScroll.js
@@ -1,33 +1,40 @@
-import { useRef, useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-export const useInfiniteScroll = (hasMorePosts) => {
-  const infiniteScrollObserverRef = useRef(null);
+export const useInfiniteScroll = (allPosts) => {
+  const LIMIT = 3;
+  const totalPosts = allPosts.length;
   const [pageNumber, setPageNumber] = useState(1);
+  const [observerRef, setObserverRef] = useState(null);
+  const feed = allPosts.slice(0, pageNumber * LIMIT);
+  const hasMorePosts = pageNumber < Math.ceil(totalPosts / pageNumber);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
-        entries.forEach((entry) => {
-          if (hasMorePosts && entry.isIntersecting) {
-            setPageNumber((prevPage) => prevPage + 1);
-          }
-        });
+        if (hasMorePosts && entries[0].isIntersecting) {
+          setTimeout(() => setPageNumber((prevPage) => prevPage + 1), 800);
+        }
       },
       { threshold: 1 }
     );
 
-    const node = infiniteScrollObserverRef.current;
-
-    if (node) {
-      observer.observe(node);
+    if (observerRef) {
+      observer.observe(observerRef);
     }
 
     return () => {
-      if (node) {
-        observer.unobserve(node);
+      if (observerRef) {
+        observer.unobserve(observerRef);
       }
     };
-  }, [hasMorePosts]);
+  }, [hasMorePosts, observerRef]);
 
-  return { pageNumber, setPageNumber, infiniteScrollObserverRef };
+  return {
+    feed,
+    hasMorePosts,
+    pageNumber,
+    setPageNumber,
+    observerRef,
+    setObserverRef,
+  };
 };
