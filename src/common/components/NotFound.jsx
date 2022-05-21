458a45c7e4707e5554e2d0984783e5c98ffd84ce
Rohan Mathur <mathurrohan04@gmail.com>
2022-05-21 11:54:08 +0530

    fix(style): not found page

--- src/common/components/NotFound.jsx
+++ src/common/components/NotFound.jsx
@@ -4,7 +4,7 @@ import notFound from "../../assets/images/notFound.png";
 
 export const NotFound = () => {
   return (
-    <main className="mt-[4rem] px-2 min-h-[calc(100vh-4rem)] bg-white dark:bg-gray-900 flex flex-col items-center justify-center">
+    <main className="pt-[2rem] px-2 min-h-screen bg-white dark:bg-gray-900 flex flex-col items-center justify-center">
       <img src={notFound} alt="notFound" className="w-1/2" />
       <h4 className="font-bold my-2 dark:text-white">
         Oops 404, page not found.
